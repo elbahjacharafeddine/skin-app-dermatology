96094bc93387f51787287ebf11707a2b82cb9de6
elbahjacharafeddine <charafensaj@gmail.com>
2023-11-18 11:22:01 +0100

    elbahja up

--- src/main/webapp/app/shared/reducers/authentication.ts
+++ src/main/webapp/app/shared/reducers/authentication.ts
@@ -141,9 +141,8 @@ export const AuthenticationSlice = createSlice({
       }))
       .addCase(getAccount.fulfilled, (state, action) => {
         const isAuthenticated = action.payload && action.payload.data && action.payload.data.activated;
-        console.log('ELBAHJA Charafeddine hello world !!');
-        console.log(action.payload.data);
-        Storage.session.set('role', action.payload.data);
+
+        Storage.session.set('user_data', action.payload.data);
         return {
           ...state,
           isAuthenticated,
